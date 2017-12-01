e9db149d5a3e17cb4cc28c9b8ae4ba4096fbd4f8
23448 <23448@ma-web.nl>
2017-12-01 10:57:31 +0100

    first Push

---
+++ VerticalSlides_Worms2/Assets/Scripts/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+    RectTransform weaponMenu;
+    Vector2 startPosition;
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        weaponMenu = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        startPosition = transform.position;
+        speed = -10f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0f, speed, 0f);
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.position = new Vector2(startPosition.x, startPosition.y);
+        }
+	}
+}
