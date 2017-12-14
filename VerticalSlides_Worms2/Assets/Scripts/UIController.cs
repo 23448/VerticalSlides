a0af47d8a898d532bafd805e901270e0f638efe2
23448 <23448@ma-web.nl>
2017-12-14 10:09:30 +0100

    Initial commit

--- VerticalSlides_Worms2/Assets/Scripts/UIController.cs
+++ VerticalSlides_Worms2/Assets/Scripts/UIController.cs
@@ -6,21 +6,37 @@ public class UIController : MonoBehaviour {
     RectTransform weaponMenu;
     Vector2 startPosition;
     public float speed;
+    public bool menuIsUp;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         weaponMenu = GameObject.Find("Canvas").GetComponent<RectTransform>();
         startPosition = transform.position;
-        speed = -10f;
+        speed = 0f;
+        menuIsUp = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(0f, speed, 0f);
-        if (Input.GetMouseButtonDown(1))
+        Debug.Log(transform.position);
+        if (Input.GetMouseButtonDown(1) && menuIsUp == false)
         {
-            transform.position = new Vector2(startPosition.x, startPosition.y);
+            menuIsUp = true;
         }
-	}
+        else if (Input.GetMouseButtonDown(1) && menuIsUp == true)
+        {
+            menuIsUp = false;
+        }
+        if (menuIsUp == false && transform.position.x < 2080)
+        {
+            speed = 10f;
+            transform.Translate(speed, 0f, 0f);
+        }
+        if (menuIsUp == true && transform.position.x > 1700)
+        {
+            speed = -10f;
+            transform.Translate(speed, 0f, 0f);
+        }
+    }
 }
